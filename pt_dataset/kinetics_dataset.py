3d9263576f324be6ef20efccac3866fa9e3ccb59
zeal-lab-ubuntu <1156478780@qq.com>
2019-01-07 09:18:18 +0800

    i3d initial

--- pt_dataset/kinetics_dataset.py
+++ pt_dataset/kinetics_dataset.py
@@ -70,10 +70,10 @@ class Kinetics(data.Dataset):
         else:
             frame_indices = self._get_indices(data)
 
-        print(len(frame_indices))
+        # print(len(frame_indices))
         video = self._frames_loader(path, frame_indices) 
         # T, C, H, W
-        print(len(video))
+        # print(len(video))
 
         if self.transform is not None:
             video = self.transform(video)
